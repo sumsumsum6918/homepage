a88a4a58be3e59cf10b8defd29c223a88051790b
Baby <tinafongfong@gmail.com>
2024-10-03 14:38:47 +0200

    added js for inner menu button to close menu bar

--- main.js
+++ main.js
@@ -2,8 +2,17 @@ document.querySelector(".menu-button").addEventListener("click", () => {
   openDropdownMenu();
 });
 
+document.querySelector(".inner-menu-button").addEventListener("click", () => {
+  closeDropdownMenu();
+});
+
 function openDropdownMenu() {
   document
     .querySelector(".menu-dropdown")
     .classList.add("js-menu-dropdown-open");
+}
+function closeDropdownMenu() {
+  document
+    .querySelector(".menu-dropdown")
+    .classList.remove("js-menu-dropdown-open");
 }
